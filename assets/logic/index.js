c64a1da6c5389c34dfc87a21c5e8bf542864613b
JT <jstorbit@gmail.com>
2025-11-05 23:21:28 -0700

    Add conditional to add odd nums to array and return odd nums array

--- assets/logic/index.js
+++ assets/logic/index.js
@@ -94,8 +94,11 @@ function getOdds(numbers) {
   let oddNums = [];
 
   for (let i = 0; i < numbers.length; i++) {
-    
+    if (numbers[i] % 2 !== 0) {
+      oddNums.push(numbers[i]);
+    }
   }
+  return oddNums;
 }
 
 // === READ BUT DO NOT EDIT THE CODE BELOW ===
