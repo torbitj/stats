9a29a8cada9cfaa22145d7e564f1fd9ee4a8588d
JT <jstorbit@gmail.com>
2025-11-05 22:45:29 -0700

    Add conditional to set max value when compared to array value and return max value

--- assets/logic/index.js
+++ assets/logic/index.js
@@ -54,8 +54,11 @@ function getMax(numbers) {
   let max = numbers[0];
 
   for (let i = 0; i < numbers.length; i++) {
-    
+    if (max < numbers[i]) {
+      max = numbers[i];
+    }
   }
+  return max;
 }
 
 /**
