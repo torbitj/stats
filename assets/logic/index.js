121792733977e7b9baa7502152e2bfb62dc0a7c4
JT <jstorbit@gmail.com>
2025-11-05 22:35:10 -0700

    Add conditional to for loop to find min value and return it

--- assets/logic/index.js
+++ assets/logic/index.js
@@ -39,8 +39,11 @@ function getMin(numbers) {
   let min = numbers[0];
 
   for (let i = 0; i < numbers.length; i++) {
-    
+    if (min > numbers[i]) {
+      min = numbers[i];
+    }
   }
+  return min;
 }
 
 /**
